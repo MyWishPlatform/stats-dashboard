96273c317a1443380322eb2719cbb290660f6781
dimanok <kovalyov1987@gmail.com>
2018-03-26 14:14:10 +0300

    Added price change for wish/eth

--- app/js/main.js
+++ app/js/main.js
@@ -6,15 +6,22 @@ angular.module('Constants', []);
 
 var module = angular.module('app', ['Constants', 'ui.router', 'Directives', 'Services', 'Filters', 'templates']);
 
-
 module.controller('baseController', function($scope, $rootScope, API_CONSTANTS, $http, $interval) {
     var updateStatistics = function() {
         $http.get(API_CONSTANTS.HOSTS.PATH + API_CONSTANTS.GET_STATISTICS).then(function(response) {
             $rootScope.statistics = response.data;
+
+            response.data.wish_price_usd = response.data.wish_price_eth * response.data.eth_price_usd;
+
             var oldWishPrice = response.data.wish_price_usd / (100 + response.data.wish_usd_percent_change_24h) * 100;
             var oldEthPrice = response.data.eth_price_usd / (100 + response.data.eth_percent_change_24h) * 100;
+
+
             var oldWishEthPrice = oldWishPrice/oldEthPrice;
-            var percentChangeWishEth = ((response.data.wish_price_usd / response.data.eth_price_usd) - oldWishEthPrice) / oldWishEthPrice * 100;
+            var newWishEthPrice = response.data.wish_price_usd / response.data.eth_price_usd;
+
+            var percentChangeWishEth = (newWishEthPrice - oldWishEthPrice) / oldWishEthPrice * 100;
+
             response.data.wish_eth_percent_change_24h = Math.round(percentChangeWishEth * 100) / 100;
             response.data.wish_price_usd = Math.round(response.data.wish_price_usd * 100) / 100;
             response.data.wish_price_eth = Math.round(response.data.wish_price_eth * 100000) / 100000;
