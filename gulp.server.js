9f71fa39de575198671ccb61f315e9ca7e8effba
dimanok <kovalyov1987@gmail.com>
2018-04-06 12:28:58 +0300

    Fixed logo Fixed font-size for body

--- gulp.server.js
+++ gulp.server.js
@@ -14,7 +14,7 @@ var devServerApi = {
     path: [
         "/api", '/accounts', '/logout'
     ],
-    url: url.parse("https://contracts.mywish.io")
+    url: url.parse("stats.mywish.io/")
     // url: url.parse("http://192.168.10.203:8000")
     // url: url.parse("http://lastwill.ddgcorp.ru")
 };
