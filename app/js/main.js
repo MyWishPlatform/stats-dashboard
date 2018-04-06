9ed3d58e1790335bdd81ace3328f0e6c4a2acb1e
dimanok <kovalyov1987@gmail.com>
2018-04-06 15:58:20 +0300

    Added ropsten network

--- app/js/main.js
+++ app/js/main.js
@@ -7,24 +7,35 @@ angular.module('Constants', []);
 var module = angular.module('app', ['Constants', 'ui.router', 'Directives', 'Services', 'Filters', 'templates']);
 
 module.controller('baseController', function($scope, $rootScope, API_CONSTANTS, $http, $interval) {
+
+    var networksList = ['ETHEREUM_MAINNET', 'ETHEREUM_ROPSTEN'];
+    $rootScope.network = 'ETHEREUM_MAINNET';
+
+    $interval(function() {
+        var index = networksList.indexOf($rootScope.network);
+        var nextIndex = index + 1;
+        nextIndex = nextIndex >= networksList.length ? 0 : nextIndex;
+        $rootScope.network = networksList[nextIndex];
+    }, 15000);
+
     var updateStatistics = function() {
         $http.get(API_CONSTANTS.HOSTS.PATH + API_CONSTANTS.GET_STATISTICS).then(function(response) {
             $rootScope.statistics = response.data;
 
-            response.data.wish_price_usd = response.data.wish_price_eth * response.data.eth_price_usd;
+            response.data.currency_statistics.wish_price_usd = response.data.currency_statistics.wish_price_eth * response.data.currency_statistics.eth_price_usd;
 
-            var oldWishPrice = response.data.wish_price_usd / (100 + response.data.wish_usd_percent_change_24h) * 100;
-            var oldEthPrice = response.data.eth_price_usd / (100 + response.data.eth_percent_change_24h) * 100;
+            var oldWishPrice = response.data.currency_statistics.wish_price_usd / (100 + response.data.currency_statistics.wish_usd_percent_change_24h) * 100;
+            var oldEthPrice = response.data.currency_statistics.eth_price_usd / (100 + response.data.currency_statistics.eth_percent_change_24h) * 100;
 
 
             var oldWishEthPrice = oldWishPrice/oldEthPrice;
-            var newWishEthPrice = response.data.wish_price_usd / response.data.eth_price_usd;
+            var newWishEthPrice = response.data.currency_statistics.wish_price_usd / response.data.currency_statistics.eth_price_usd;
 
             var percentChangeWishEth = (newWishEthPrice - oldWishEthPrice) / oldWishEthPrice * 100;
 
-            response.data.wish_eth_percent_change_24h = Math.round(percentChangeWishEth * 100) / 100;
-            response.data.wish_price_usd = Math.round(response.data.wish_price_usd * 100) / 100;
-            response.data.wish_price_eth = Math.round(response.data.wish_price_eth * 100000) / 100000;
+            response.data.currency_statistics.wish_eth_percent_change_24h = Math.round(percentChangeWishEth * 100) / 100;
+            response.data.currency_statistics.wish_price_usd = Math.round(response.data.currency_statistics.wish_price_usd * 100) / 100;
+            response.data.currency_statistics.wish_price_eth = Math.round(response.data.currency_statistics.wish_price_eth * 100000) / 100000;
         });
     };
     $interval(updateStatistics, 30000);
